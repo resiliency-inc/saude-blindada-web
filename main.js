9d759add9fa8823f7b44b11e7c79ab3f5677b573
Anderson <anderson.araujo@ssp.ba.gov.br>
2020-03-12 09:28:48 -0300

    changes

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-import { API_URL } from "./variables.mjs";
-
 const button = document.querySelector('#enviar');
+const API_URL = "aHR0cHM6Ly9zYXVkZS1ibGluZGFkYS1hcGkuaGVyb2t1YXBwLmNvbS8=";
 
 button.onclick = async function () {
     const form = document.querySelector('form');
     const email = document.querySelector('#email').value;
     const phone = document.querySelector('#phone').value;
+    const load = document.querySelector('#loader');
 
     if (form.checkValidity()) {
         try {
+            load.setAttribute("class", "loader");
             const res = await fetch(`${window.atob(API_URL)}clients/create`, {
                 method: 'post',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, phone })
             });
-
             if (res.status === 201) {
                 const data = await res.json();
                 const alert = document.querySelector(".alert");
@@ -27,7 +27,9 @@ button.onclick = async function () {
                 alert.innerHTML = data;
                 alert.setAttribute("class", "alert alert-danger");
             }
+            load.removeAttribute("class", "loader");
         } catch (error) {
+            load.removeAttribute("class", "loader");
             const alert = document.querySelector(".alert");
             alert.innerHTML = error.error || "Ocorreu um erro";
             alert.setAttribute("class", "alert alert-danger");
--- variables.mjs
+++
@@ -1,1 +0,0 @@
-export const API_URL = "aHR0cHM6Ly9zYXVkZS1ibGluZGFkYS1hcGkuaGVyb2t1YXBwLmNvbS8=";
